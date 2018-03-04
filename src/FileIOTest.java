a52d36ca5c25c45d3d5f6f9371542e92f8ddf220
Gene Lee <leexgenex@gmail.com>
2018-03-04 11:48:06 -0600

    Sum

--- src/FileIOTest.java
+++ src/FileIOTest.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+import java.io.*;
+
 public class FileIOTest {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	Scanner input = new Scanner(System.in);
+	System.out.println("Enter name of data file.");
+		String name = input.nextLine();
 		
+	Scanner file = new Scanner(name);
+	int sum = 0;
+	while (file.hasNext()){
+		sum+=file.nextInt();
+	}
+	System.out.println("Sum: "+sum);
+	
 	}
 
 }
